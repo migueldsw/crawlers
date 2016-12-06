b1a02a4ef75839cc6f3fcd344383c6e47216a0c7
Miguel DSW <mdsw@cin.ufpe.br>
2016-12-05 22:14:36 -0300

    new spider

--- mycrawlers/main.py
+++ mycrawlers/main.py
@@ -8,6 +8,8 @@ import numpy as np
 import os
 #call(['ls','-la'])
 
+#run: scrapy crawl spider_1
+
 #-----------------------
 DOCS_DIR = 'out/'
 # UTILS------------------------------------------
--- mycrawlers/mycrawlers/spiders/prs_spider.py
+++ mycrawlers/mycrawlers/spiders/prs_spider.py
@@ -26,8 +26,19 @@ def htm2txt(htm):
 	frag = BS(htm)
 	return frag.get_text()
 
-INCLUSION_WORDS = ['/noticia']
-EXCLUSION_WORDS = ['busca/?q']
+def remove_trash_itens(itens_lst,words_lst):
+	out = []
+	if words_lst == []:
+		return itens_lst
+	for i in itens_lst:
+		ok = True
+		for w in words_lst:
+			if w in i:
+				ok = False
+		if ok:
+			out.append(i)
+	return out
+
 
 def check_url(url,inclusion_words_list,exclusion_words_list):
 	#the given url must have at least one of the inclusion words, and none of the exclusion words.
@@ -58,6 +69,9 @@ class PRSpider(scrapy.Spider):
 		]
 	CONT = 0
 
+	INCLUSION_WORDS = ['/noticia']
+	EXCLUSION_WORDS = ['busca/?q']
+
 	def parse(self, response):
 		rr = response
 		perguntas = rr.xpath('//header/h3/text()').extract()
@@ -82,19 +96,22 @@ class Spider1(CrawlSpider):
 	allowed_domains = ['jconline.ne10.uol.com.br']
 	start_urls = [
 		'http://jconline.ne10.uol.com.br/',
-		'http://jconline.ne10.uol.com.br/canal/cidades/geral/noticia/2016/11/23/rodoviarios-nao-vao-aderir-a-paralisacao-nacional-do-dia-25-261416.php'
+		#'http://jconline.ne10.uol.com.br/canal/cidades/geral/noticia/2016/11/23/rodoviarios-nao-vao-aderir-a-paralisacao-nacional-do-dia-25-261416.php'
 		]
 	rules = (Rule(LinkExtractor(allow=(), ), callback="parse_page", follow= True),)
 
 	CONT = 0
+	INCLUSION_WORDS = ['/noticia']
+	EXCLUSION_WORDS = ['busca/?q']
+	TRASH_WORDS = ['Todos os direitos reservados','Recomendados para voc','Publicado em']
 
 	def parse_page(self, response):
 		#crawler doc limit
-		CRAW_LIMIT = 3
+		CRAW_LIMIT = 10
 		if (Spider1.CONT>=CRAW_LIMIT):
 			raise CloseSpider('docs_limit_exceeded')
 		rr = response
-		if check_url(rr.url,INCLUSION_WORDS,EXCLUSION_WORDS):
+		if check_url(rr.url,Spider1.INCLUSION_WORDS,Spider1.EXCLUSION_WORDS):
 
 			appendFile('URLS.log',rr.url)
 
@@ -105,6 +122,7 @@ class Spider1(CrawlSpider):
 			itensRaw += rr.xpath('//h3/text()').extract()
 			itensRaw += rr.xpath('//p/text()').extract()
 
+			itensRaw = remove_trash_itens(itensRaw,Spider1.TRASH_WORDS)
 			#print ('LEN : '+str(len(itensRaw)))
 			itensOut = []
 			outdir = './out'
@@ -114,5 +132,57 @@ class Spider1(CrawlSpider):
 				txt = htm2txt(i.encode('utf8'))
 				if (len(txt) >= 20): #string min. length
 					itensOut.append(txt)
-					appendFile('out/DOC-'+str(Spider1.CONT),txt)
+					appendFile('out/DOC-JC-'+str(Spider1.CONT),txt)
 			Spider1.CONT+=1
+
+class Spider2(CrawlSpider):
+	name = "spider_2"
+	allowed_domains = ['www.diariodepernambuco.com.br']
+	start_urls = [
+		'http://www.diariodepernambuco.com.br/app/noticia/vida-urbana/2016/12/05/interna_vidaurbana,678505/assalto-no-bompreco-de-olinda-termina-com-feridos-e-presos.shtml',
+		'http://www.diariodepernambuco.com.br/',
+		]
+	rules = (Rule(LinkExtractor(allow=(), ), callback="parse_page", follow= True),)
+
+	CONT = 0
+	INCLUSION_WORDS = ['/noticia']
+	EXCLUSION_WORDS = ['busca/?q']
+	TRASH_WORDS = ['bua de mar','todos os direitos reservados.','Copyright Jornal Diario de Pernambuco', ' | 1']
+
+	def parse_page(self, response):
+		#crawler doc limit
+		CRAW_LIMIT = 30
+		if (Spider2.CONT>=CRAW_LIMIT):
+			raise CloseSpider('docs_limit_exceeded')
+		rr = response
+		if check_url(rr.url,Spider2.INCLUSION_WORDS,Spider2.EXCLUSION_WORDS):
+
+			appendFile('URLS.log',rr.url)
+
+			itensRaw = []
+			
+			itensRaw += rr.xpath('//h1/text()').extract()
+			itensRaw += rr.xpath('//h2/text()').extract()
+			itensRaw += rr.xpath('//h3/text()').extract()
+			itensRaw += rr.xpath('//br/text()').extract()
+
+			itensRaw += rr.xpath('//div/text()').extract()
+			itensRaw += rr.xpath('//em/text()').extract()
+			itensRaw += rr.xpath('//strong/text()').extract()
+			itensRaw += rr.xpath('//span[@class="h1"]/text()').extract()
+			itensRaw += rr.xpath('//span[@class="gallery_desc"]/text()').extract()
+			itensRaw += rr.xpath('//p/text()').extract()
+
+			itensRaw = remove_trash_itens(itensRaw,Spider2.TRASH_WORDS)
+
+			#print ('LEN : '+str(len(itensRaw)))
+			itensOut = []
+			outdir = './out'
+			if not os.path.isdir(outdir):
+				os.makedirs(outdir)
+			for i in itensRaw:
+				txt = htm2txt(i.encode('utf8'))
+				if (len(txt) >= 20): #string min. length
+					itensOut.append(txt)
+					appendFile('out/DOC-DP-'+str(Spider2.CONT),txt)
+			Spider2.CONT+=1
